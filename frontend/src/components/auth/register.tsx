322a90889ad1f1e167070c946271bb45449a91a4
rwiiin <rwiiinzakeri@gmail.com>
2025-01-02 20:20:40 +0330

    [addFeature] work on digram and register

--- frontend/src/components/auth/register.tsx
+++ frontend/src/components/auth/register.tsx
@@ -1,12 +1,14 @@
 import CustomeButton from "@/module/button/Button";
 import DefaultInput from "@/module/inputs/inputDefaut";
 import { AuthFormProps, AuthType } from "@/types/global";
-import Form from "next/form";
 
 const Register = ({ setPosition }: AuthFormProps) => {
+
+  
+
   return (
     <>
-      <Form className="flex flex-col gap-3">
+      <form className="flex flex-col gap-3">
         <div className="flex gap-3">
           <DefaultInput placeholder="user name" />
           <DefaultInput placeholder="email" />
@@ -14,7 +16,7 @@ const Register = ({ setPosition }: AuthFormProps) => {
         <DefaultInput placeholder="password" />
         <DefaultInput placeholder="repassword" />
         <CustomeButton text="Register" type={3} />
-      </Form>
+      </form>
       <p
         onClick={() => setPosition(AuthType.login)}
         className="text-xs mt-4 cursor-pointer w-fit"
